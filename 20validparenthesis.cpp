d142017d6b893b915e54b1664c88f7f21b8cb991
Ali Taha <aliestaha04@gmail.com>
2023-06-19 08:43:32 -0400

    -

--- 20validparenthesis.cpp
+++ 20validparenthesis.cpp
@@ -11,20 +11,26 @@ public:
 
         for (char c : s)
         {
+            // parentheses.count(c) is checking whether the character
+            // c exists as a key in the parentheses map
             if (parentheses.count(c))
             {
+                // If stack is empty or stack.top() isn't the opening of that parentheses
                 if (st.empty() || st.top() != parentheses[c])
                 {
                     return false;
                 }
+                // Otherwise remove it from the stack
                 st.pop();
             }
             else
             {
+                // If it's an open brackets{} push it
                 st.push(c);
             }
         }
 
+        // True if stack is empty, false otherwise
         return st.empty();
     }
 };
