78d8b4442493c094365ff260acb9cfee07b481b0
Ali Taha <ataha.blackberry.com>
2023-07-12 13:04:27 -0400

    -

--- 11watercontainer.cpp
+++ 11watercontainer.cpp
@@ -1,3 +1,62 @@
+/*
+11. Container With Most Water
+Medium
+You are given an integer array height of length n. There are n vertical lines drawn such that
+the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+Ex) 1
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water
+(blue section) the container can contain is 49.
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+
+
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+
+    }
+};
+
+*/
+
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        for (int i = 0; i < height.size()-1; i++)
+        {
+            int present = height[i];
+            int max = 0;
+            int area =0;
+
+            for (int j = i+1; j < height.size()-1; j++)
+            {
+                int next=height[j];
+                int length= j-i;
+                int area= (present<next) ? present* length : next*length;
+                if (max<area){
+                    max=area;
+                }
+
+            }
+        }
+        return max;
+    }
+};
+
 // Optimized solution
 class Solution
 {
--- 1twosum.cpp
+++ 1twosum.cpp
@@ -1,4 +1,53 @@
-// https://leetcode.com/problems/two-sum/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        //Make a hashmap, one of them will have the numbers, one will have the index
+        unordered_map<int, int> dic;        
+        
+        for (int i=0; i< nums.size(); i++){
+            int num= nums[i];
+            int goal = target - num;
+
+            //iterator points to the dictionary key value pair. it->first and it->second is the value
+            auto it= dic.find(goal);
+
+            if (it!=dic.end()){
+                return {it->second, i};
+            }else{
+                dic[num]=i;
+            }
+            }
+            return {};
+        }
+    };
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+////////////////////
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+    }
+};
+////////////////////
+*/
 
 // Solution Hashmaps
 class Solution
@@ -6,7 +55,6 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
-
         // Unordered map to store numbers and their indices
         unordered_map<int, int> dic;
 
@@ -17,13 +65,13 @@ public:
             int goal = target - num;
 
             // Check if the 'goal' value exists in the map
-            auto isfound = dic.find(goal);
+            auto it = dic.find(goal);
 
-            if (isfound != dic.end())
+            if (it != dic.end())
             {
                 // 'goal' value found in the map
                 // Return a vector with the indices of the two numbers
-                return {isfound->second, i};
+                return {it->second, i};
             }
             else
             {
@@ -94,3 +142,7 @@ public:
         return result; // Empty vector if no solution found
     }
 };
+
+
+
+
