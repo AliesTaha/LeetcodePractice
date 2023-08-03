e1d6ebec3c0eff7c4c432f297957f458440778d7
Ali Taha <aliestaha04@gmail.com>
2023-08-03 19:34:14 -0400

    what

--- week3/542-01-matrix.cpp
+++ week3/542-01-matrix.cpp
@@ -1,6 +1,6 @@
 /*
 542. 01 Matrix
-Given an m x n binary matrix mat, return
+Given an rows x columns binary matrix mat, return
 the distance of the nearest 0 for each cell.
 
 The distance between two adjacent cells is 1.
@@ -21,46 +21,66 @@ Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
 Output: [[0,0,0],[0,1,0],[1,2,1]]
 
 */
+#include <vector>
+#include <queue>
+
 class Solution
 {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>> &mat)
     {
-        int rows = mat.size();
-        int columns = mat[0].size();
+        int rows = mat.size();       // Number of rows in the matrix
+        int columns = mat[0].size(); // Number of columns in the matrix
 
-        vector<vector<int>> result[rows][columns];
+        // Create an output matrix of the same size initialized with -1
+        vector<vector<int>> result(rows, vector<int>(columns, -1));
 
-        for (int i = 0; i < rows * columns; i++)
+        // Define directions to move: up, down, left, right
+        vector<int> dx = {-1, 1, 0, 0}; //"dx" contains the values {-1, 1, 0, 0}, which correspond to the movements {-1, 0} (left), {1, 0} (right)
+        vector<int> dy = {0, 0, -1, 1}; //"dy" contains the values {0, 0, -1, 1}, which correspond to the movements {0, -1} (up), {0, 1} (down)
+
+        // Create a queue for BFS
+        queue<pair<int, int>> q;
+
+        // First pass: starting from 0 cells, mark them as 0 and add them to the queue
+        for (int i = 0; i < rows; i++)
         {
+            for (int j = 0; j < columns; j++)
+            {
+                if (mat[i][j] == 0)
+                {
+                    result[i][j] = 0;
+                    q.push({i, j});
+                }
+            }
         }
 
-        // if top edge
-        else if (i <= rows)
+        // Second pass: perform BFS to find distances from each 0 cell to 1 cells
+        while (!q.empty())
         {
-            // if top right corner
-            // if top left corner
+            auto curr = q.front();
+            q.pop();
+
+            int x = curr.first;
+            int y = curr.second;
+
+            // Explore adjacent cells
+            for (int k = 0; k < 4; k++)
+            {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+
+                // Check if the new cell is within the matrix bounds
+                if (nx >= 0 && nx < rows && ny >= 0 && ny < columns && result[nx][ny] == -1)
+                {
+                    // Update the distance and add the new cell to the queue
+                    result[nx][ny] = result[x][y] + 1;
+                    q.push({nx, ny});
+                }
+            }
         }
 
-        // if bottom edge
-        else if (i >= rows * (columns - 1))
-        {
-            // if bottom right corner
-            // if bottom left corner
-        }
-
-        // if left edge
-        else if (i % columns == 0)
-        {
-        }
-
-        // if right edge
-        else if ((i + 1) % columns == 0)
-        {
-        }
-
-        else
-        {
-        }
+        // Return the final result matrix
+        return result;
     }
 };
